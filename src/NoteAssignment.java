36f674a2dc70aaf60b85c672183948ec135b4526
E3Ha <emma.harrison@talawanda.org>
2024-04-03 14:04:19 -0400

    4/3/2024 2:05

--- src/NoteAssignment.java
+++ src/NoteAssignment.java
@@ -1,5 +1,26 @@
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+//Imports every utility
+//import java.util.*;
+
+public class NoteAssignment {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int [] myArray = new int [100];
+        int count = 0;
+
+        for (int i = 0; i < 100; i++){
+            myArray[i] = count;
+            count +=2;
+
+        }
+        for (int i = 0; i < 100; i++){
+            System.out.print(myArray[i] + " | ");
+
+        }
+
     }
 }
